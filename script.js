4d548b72f8e3b47dca689af3c66b174756f4b4cd
JKK2012 <kogeyjimmy@gmail.com>
2025-04-22 10:36:43 +0300

    update

---
+++ script.js
@@ -0,0 +1,15 @@
+// variables
+//const-this is used with constant variables that cannot be changed//
+const pi = 3.14; // number
+// let-this is used with variable that can be changed//
+let age = 30; // number
+// data types
+// type of value assigned to a `variable name
+// types of data include: string, number, boolean, null, undefined, object, array
+// string- a sequence of characters enclosed in quotes
+// number- a number with or without decimal points
+// boolean- true or false
+// null- a value that is empty or non-existent
+// undefined- a variable that has been declared but not assigned a value
+// object- a collection of key-value pairs enclosed in curly braces
+// array- a collection of values enclosed in square brackets
